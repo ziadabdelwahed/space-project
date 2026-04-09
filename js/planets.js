32e69485f1f7ca6aee85e7deb18ae1a5764deb6d
ziadabdelwahed <zabdelwahed930@gmail.com>
2026-04-09 14:52:28 +0200

    Update THREE.js import to use CDN link

--- js/planets.js
+++ js/planets.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.128.0/build/three.module.js';
 
 class Planet {
     constructor(radius, color, distance, speed, hasRing = false, ringColor = 0xaaaaaa) {
