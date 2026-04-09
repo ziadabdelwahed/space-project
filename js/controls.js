bceb8c78a77aeb88521521a747903adb3fe38f70
ziadabdelwahed <zabdelwahed930@gmail.com>
2026-04-09 14:52:01 +0200

    Update THREE.js import to use CDN link

--- js/controls.js
+++ js/controls.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.128.0/build/three.module.js';
 
 export function setupRaycaster(camera, domElement, objects, updateUICallback) {
     const raycaster = new THREE.Raycaster();
