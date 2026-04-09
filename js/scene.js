c020eeff103803b324e749a183c22cf1b38a592f
ziadabdelwahed <zabdelwahed930@gmail.com>
2026-04-09 14:52:52 +0200

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.128.0/build/three.module.js';
 
 export function createGalaxyBackground(scene) {
     // 1. حقل نجوم بعيد (Particle System)
